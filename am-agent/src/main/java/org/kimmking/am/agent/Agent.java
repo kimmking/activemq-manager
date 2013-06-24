8889708e6504cf37f3374601d40d344f47e6688b
kimmking <kimmking.cn@gmail.com>
2013-06-24 21:34:46 +0800

    add jmx

--- am-agent/src/main/java/org/kimmking/am/agent/Agent.java
+++ am-agent/src/main/java/org/kimmking/am/agent/Agent.java
@@ -1,14 +1,17 @@
 package org.kimmking.am.agent;
 
+
 /**
  * @author kimmking.cn@gmail.com
  */
-public class Agent {
-	
-	public void start(String[] args){
+public class Agent implements AgentMBean{
+
+	@Override
+	public String hello(String test) {
 		
+		return "Hello,"+test;
 	}
-	
+
 	
 
 }
--- am-agent/src/main/java/org/kimmking/am/agent/Main.java
+++ am-agent/src/main/java/org/kimmking/am/agent/Main.java
@@ -1,12 +1,30 @@
 package org.kimmking.am.agent;
 
+import javax.management.MBeanServer;
+import javax.management.MBeanServerFactory;
+import javax.management.ObjectName;
+
+import com.sun.jdmk.comm.HtmlAdaptorServer;
+
 /**
  * @author kimmking.cn@gmail.com
  */
 public class Main {
 
-	public static void main(String[] args) {
-		new Agent().start(args);
+	public static void main(String[] args) throws Exception {
+
+			MBeanServer server = MBeanServerFactory.createMBeanServer();
+
+			ObjectName helloName = new ObjectName("org.kimmking.am:name=Agent");
+			server.registerMBean(new Agent(), helloName);
+
+			ObjectName adapterName = new ObjectName("org.kimmking.am:name=htmladapter,port=8082");
+			HtmlAdaptorServer adapter = new HtmlAdaptorServer();
+			server.registerMBean(adapter, adapterName);
+
+			adapter.start();
+			System.out.println("start.....");
+
 	}
 
 }
