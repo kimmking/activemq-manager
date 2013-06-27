34fd26d32065f0bd9dcb503819db3e64be244e89
kimmking <kimmking.cn@gmail.com>
2013-06-27 17:11:24 +0800

    add shell

---
+++ am-agent/bin/startActiveMQ.sh
@@ -0,0 +1,27 @@
+echo $1
+export WORK_PATH=$1
+export WORK_VERSION=$2
+if [ -z "$WORK_PATH" ]; then
+	WORK_PATH=`pwd`/mq
+fi
+if [ -z "$WORK_VERSION" ]; then
+	WORK_VERSION="5.7.0"
+fi
+
+if [ ! -d "$WORK_PATH" ]; then  
+	mkdir $WORK_PATH  
+fi  
+
+cd $WORK_PATH
+
+if [ ! -f "$WORK_PATH/apache-activemq-$WORK_VERSION.zip" ]; then  
+	wget http://192.168.1.201/apache-activemq-$WORK_VERSION.zip
+	chmod 777 apache-activemq-$WORK_VERSION.zip
+fi 
+if [ ! -d "$WORK_PATH/apache-activemq-$WORK_VERSION" ]; then  
+	unzip apache-activemq-$WORK_VERSION.zip
+	chmod 777 apache-activemq-$WORK_VERSION -R
+fi 
+
+cd apache-activemq-$WORK_VERSION
+./bin/activemq start
---
+++ am-agent/bin/stopActiveMQ.sh
@@ -0,0 +1,18 @@
+echo $1
+export WORK_PATH=$1
+export WORK_VERSION=$2
+if [ -z "$WORK_PATH" ]; then
+	WORK_PATH=`pwd`/mq
+fi
+if [ -z "$WORK_VERSION" ]; then
+	WORK_VERSION="5.7.0"
+fi
+
+if [ ! -d "$WORK_PATH" ]; then  
+	mkdir $WORK_PATH  
+fi  
+
+cd $WORK_PATH
+
+cd apache-activemq-$WORK_VERSION
+./bin/activemq stop
